1a2b69ea83cca06dcbcc98a084f9db9b6f8060c3
jack <1286698629@qq.com>
2019-09-25 11:20:28 +0800

    merge

--- CORE_C02_011/src/DEMO.java
+++ CORE_C02_011/src/DEMO.java
@@ -7,8 +7,6 @@ public class DEMO {
 	 Scanner sc=new Scanner(System.in);
 	for(;;){
 		 
-
-		
 		System.out.println("DSAD9555896555dsadsd6898999ASD");
 		System.out.println("DSADA65566565656556SD");
 
