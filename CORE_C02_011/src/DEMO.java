afb8d1c94d050ee479bef96b270e5236dc2a3f20
jack <1286698629@qq.com>
2019-09-25 17:23:31 +0800

    25

--- CORE_C02_011/src/DEMO.java
+++ CORE_C02_011/src/DEMO.java
@@ -6,7 +6,7 @@ public class DEMO {
 	int num1,num2;
 	 Scanner sc=new Scanner(System.in);
 	for(;;){
-		int i=0;
+		int i=0;int m=0;
 		 System.out.println("25DSAD955dfdsfdsfsdfsddsdfdsf589665656565656565555dsadsd6898999ASD");	
 		   System.out.println("25DSAD955dfdsfdsfsdfsddsdfdsf589665656565656565555dsadsd6898999ASD");
 	   	System.out.println("DSAD955dfdsfdsfsdfsddsdfdsf589665656565656565555dsadsd6898999ASD");
